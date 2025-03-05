3739ed0274c6ee9dce85c10dfc336f8f0cbfcd37
= <=>
2025-03-05 17:20:19 +0700

    commit index

--- api/index.js
+++ api/index.js
@@ -45,7 +45,7 @@ const minioClient = new Minio.Client({
   // region: process.env.AWS_REGION, //remove this when use local endpoint
   endPoint: process.env.ENDPOINT, // MinIO server address
   port: 9400,
-  useSSL: true,
+  useSSL: false,
   accessKey: process.env.MINIO_ACCESS_KEY_ID,
   secretKey: process.env.MINIO_SECRET_ACCESS_KEY,
 });
